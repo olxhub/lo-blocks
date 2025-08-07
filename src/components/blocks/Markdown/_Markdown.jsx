8b6d209f24c1136c83b9a682b8be502275dc1f0d
Piotr Mitros <piotr@mitros.org>
2025-08-07 14:27:39 -0400

    Removed done TODO

--- src/components/blocks/Markdown/_Markdown.jsx
+++ src/components/blocks/Markdown/_Markdown.jsx
@@ -3,6 +3,5 @@ import ReactMarkdown from 'react-markdown';
 
 export function _Markdown( props ) {
   const { kids } = props;
-  // TODO: Ignore common indent. These often have a large baseline indent due to XML formatting.
   return <ReactMarkdown>{kids}</ReactMarkdown>;
 }
