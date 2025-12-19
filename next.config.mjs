0cbef1361ad00705ae2b8e722d902132e3d0bb26
Piotr Mitros <piotr@mitros.org>
2025-12-19 08:32:19 -0500

    Standalone toggle

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@
 const nextConfig = {
   // Standalone output for containerized deployments (AWS Amplify, Docker, etc.)
   // This bundles all dependencies into .next/standalone for a self-contained server
-  output: 'standalone',
+  // output: 'standalone',
+  // Non-standalone is more robust for hosted deploys, which we're using right now
+
+  // We probably want:
+  ...(process.env.STANDALONE === 'true' && { output: 'standalone' }),
+  // So we can toggle it.
 };
 
 export default nextConfig;
